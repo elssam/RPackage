5968f09db2ea8e8dd1b828e8db5d2b8e7e86637b
Samar Elsheikh <samar.salah119@gmail.com>
2021-09-06 02:34:05 -0400

    editing fars_functions3

--- R/fars_functions3.R
+++ R/fars_functions3.R
@@ -28,8 +28,8 @@ fars_read_years <- function(years) {
     file <- make_filename(year)
     tryCatch({
       dat <- fars_read(file)
-      dplyr::mutate(dat, year = year) %>% 
-        dplyr::select(MONTH, year)
+      dplyr::mutate(dat, year = year) %>%
+        dplyr::select(.data$MONTH, .data$year)
     }, error = function(e) {
       warning("invalid year: ", year)
       return(NULL)
